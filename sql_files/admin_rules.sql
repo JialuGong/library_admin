d8fd02d293e3915a210466949e894f0c32b132e3
floatinjianghu <jianghupiao@mail.nwpu.edu.cn>
2019-10-12 22:33:40 +0800

    fix some errors about double type and add other sql used in this project such as categroy.sql

--- sql_files/admin_rules.sql
+++ sql_files/admin_rules.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 05/10/2019 21:11:24
+ Date: 12/10/2019 22:30:15
 */
 
 SET NAMES utf8mb4;
@@ -23,15 +23,31 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `admin_rules`;
 CREATE TABLE `admin_rules`  (
   `rule_id` int(11) NOT NULL AUTO_INCREMENT,
-  `fine` double(255, 0) NOT NULL,
-  `period` double(255, 0) NOT NULL,
-  `deposit` double(255, 0) NOT NULL,
+  `fine` double(255, 2) NOT NULL,
+  `period` double(255, 2) NOT NULL,
+  `deposit` double(255, 2) NOT NULL,
+  `timestamp` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`rule_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of admin_rules
 -- ----------------------------
-INSERT INTO `admin_rules` VALUES (1, 1, 30, 300);
+INSERT INTO `admin_rules` VALUES (1, 1.00, 30.00, 300.00, NULL);
+INSERT INTO `admin_rules` VALUES (2, 112.00, -1.00, -1.00, NULL);
+INSERT INTO `admin_rules` VALUES (3, -1.00, 9897.00, -1.00, NULL);
+INSERT INTO `admin_rules` VALUES (4, -1.00, -12.00, -1.00, NULL);
+INSERT INTO `admin_rules` VALUES (5, -1.00, 86776868.00, -1.00, NULL);
+INSERT INTO `admin_rules` VALUES (6, -1.00, -1.00, 139743.00, NULL);
+INSERT INTO `admin_rules` VALUES (7, -1.00, -1.00, -990.00, NULL);
+INSERT INTO `admin_rules` VALUES (8, -1.00, -1.00, 39274.00, NULL);
+INSERT INTO `admin_rules` VALUES (9, -1.00, -1.00, 3782648.00, NULL);
+INSERT INTO `admin_rules` VALUES (10, -1.00, -1.00, 234.00, NULL);
+INSERT INTO `admin_rules` VALUES (11, 112.00, -1.00, -1.00, 'hfoesaoheohfoe');
+INSERT INTO `admin_rules` VALUES (12, -1.00, 34567.00, -1.00, 'hfoesaoheohfoe');
+INSERT INTO `admin_rules` VALUES (13, -1.00, -1.00, 234342.00, 'hfoesaoheohfoe');
+INSERT INTO `admin_rules` VALUES (14, 3.00, -1.00, -1.00, 'hfoesaoheohfoe');
+INSERT INTO `admin_rules` VALUES (15, 3.43, -1.00, -1.00, 'hfoesaoheohfoe');
+INSERT INTO `admin_rules` VALUES (16, 3.43, -1.00, -1.00, 'hfoesaoheohfoe');
 
 SET FOREIGN_KEY_CHECKS = 1;
--- sql_files/librarian.sql
+++ sql_files/librarian.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 05/10/2019 21:11:31
+ Date: 12/10/2019 22:30:41
 */
 
 SET NAMES utf8mb4;
@@ -22,13 +22,21 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `librarian`;
 CREATE TABLE `librarian`  (
-  `librarian_id` int(11) NOT NULL,
+  `librarian_id` int(11) NOT NULL AUTO_INCREMENT,
   `librarian_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `librarian_password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `librarian_phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `librarian_email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`librarian_id`) USING BTREE,
   UNIQUE INDEX `librarian_name`(`librarian_name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of librarian
+-- ----------------------------
+INSERT INTO `librarian` VALUES (3, 'test123', '9', 'test', 'test');
+INSERT INTO `librarian` VALUES (4, 'test1223456', '9', 'test', 'test');
+INSERT INTO `librarian` VALUES (5, 'test123345', '9', 'test', 'test');
+INSERT INTO `librarian` VALUES (6, 'test1233457689', '9', 'test', 'test');
 
 SET FOREIGN_KEY_CHECKS = 1;
